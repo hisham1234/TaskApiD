bf24de0e150739a4410353471b11045049ecaa14
hisham1234 <hishamnawzer@gmail.com>
2021-07-23 18:40:10 +0900

    added files

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks= new[] { "Task1", "Task2" };
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return BadRequest();
+        }
+    }
+}
